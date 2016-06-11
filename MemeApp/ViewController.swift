9df85352e5fac138a2cee99fac6f6b52be2d4da3
Dan Huang <jhyqt5@gmail.com>
2016-06-11 11:30:57 -0600

    feat: Add imagePicker functionality

--- MemeApp/ViewController.swift
+++ MemeApp/ViewController.swift
@@ -8,20 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    @IBAction func getAlbum(sender: AnyObject) {
-        print("album")
+    @IBOutlet weak var memeView: UIImageView!
+    let picker = UIImagePickerController()
+    
+    @IBAction func getAlbum(sender: UIBarButtonItem) {
+        picker.allowsEditing = false
+        picker.sourceType = .PhotoLibrary
+        presentViewController(picker, animated: true, completion: nil)
     }
     
-    @IBAction func getCamera(sender: AnyObject) {
-        print("hello")
+    @IBAction func getCamera(sender: UIBarButtonItem) {
+        picker.allowsEditing = false
+        picker.sourceType = UIImagePickerControllerSourceType.Camera
+        picker.cameraCaptureMode = .Photo
+        picker.modalPresentationStyle = .FullScreen
+        presentViewController(picker, animated: true, completion: nil)
     }
     
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
-
-}
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        picker.delegate = self
+    }
+    
+    //MARK: Delegates
+    func imagePickerController( picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        memeView.contentMode = .ScaleAspectFit
+        memeView.image = selectedImage
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+} //End View Controller
 
